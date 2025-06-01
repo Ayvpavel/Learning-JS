751e11d5469d09be5d011dd95dda02479304277d
unknown <pavel.1504@mail.ru>
2025-06-02 03:09:42 +1000

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+let nime = prompt('Привет! Как тебя зовут?');
+while (nime == '' || nime == null) {
+  nime = prompt('Привет! Как тебя зовут?');
+}
+
+alert(nime + ', добро пожаловать на наш сайт, я загадал число от 1 до 100');
+
+let number = Math.ceil(Math.random() * 100);
+let guess = prompt('Какое число я загадал?');
+let numberOfGuesses = 0;
+
+while (guess != number) {
+  if (guess > number) {
+    guess = prompt('Много, попробуй ещё раз');
+    numberOfGuesses = numberOfGuesses + 1;
+  }
+  if (guess < number) {
+    guess = prompt('Мало, попробуй ещё раз');
+    numberOfGuesses = numberOfGuesses + 1;
+  }
+}
+alert(
+  'Ты угадал! Это число' +
+    ' ' +
+    number +
+    '. Тебе понадобтлось ' +
+    numberOfGuesses +
+    '  попыток.'
+);
